18bc812da66072f1a4b5de15c354f9232f4e0eaa
Meesam Zaidi <meesam.engineer@gmail.com>
2016-12-29 23:35:20 +0530

    Update UserLoginBL.cs

--- Models/UserLoginBL.cs
+++ Models/UserLoginBL.cs
@@ -38,7 +38,7 @@ namespace JobPortal.Models
                       Roles=new string[] { "Admin","Consultant" }
                  },
                  new UserRole() {
-                     UserId=3,
+                     Uid=3,
                       Roles=new string[] { "Consultant" }
                  }
             };
